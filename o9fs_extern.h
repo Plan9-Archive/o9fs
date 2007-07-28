d03eb7f9bc538510360ae9fdca27a48ac291ebbe
iru <iru@localhost>
2007-07-27 22:55:55 -0300

    just reflect the changes

--- o9fs_extern.h
+++ o9fs_extern.h
@@ -1,15 +1,13 @@
 /* o9fs_subr.c */
-int		o9fs_alloc_dirent(struct o9fsnode *, const char *, uint16_t namelen, struct o9fsdirentx **);
-int		o9fs_alloc_node(struct o9fsnode **, enum vtype);
-int		o9fs_alloc_vp(struct mount *, struct o9fsnode *, struct vnode **, u_long);
-int		o9fs_create_file(struct vnode *, struct vnode **, struct vattr *, struct componentname *);
-void	o9fs_dir_attach(struct vnode *, struct o9fsdirentx *);
-struct	o9fsdirentx *o9fs_dir_lookup(struct o9fsnode *, struct componentname *);
+int		o9fs_allocvp(struct mount *, struct o9fsfid *, struct vnode **, u_long);
+int		o9fs_insertfid(struct o9fsfid *, struct o9fsfid *);
 int		o9fs_rpc(struct o9fsmount *, struct o9fsfcall *, struct o9fsfcall *);
+struct o9fsfid *o9fs_fid_lookup(struct o9fsmount *, struct o9fsfid *, char *);
+void	o9fs_dumpchildlist(struct o9fsfid *);
 u_int	o9fs_tokenize(char **, u_int, char *, char);
 
 /* o9fs_io.c */
-int		o9fs_tcp_open(struct o9fsmount *);
+int		o9fs_tcp_connect(struct o9fsmount *);
 int		o9fs_tcp_write(struct o9fsmount *, struct uio *);
 int		o9fs_tcp_read(struct o9fsmount *, struct uio *);
 int		o9fs_tcp_close(struct o9fsmount *);
@@ -17,8 +15,8 @@ int		o9fs_tcp_close(struct o9fsmount *);
 /* o9fs_9p.c */
 int		o9fs_tversion(struct o9fsmount *, int, char *);
 struct o9fsfid *o9fs_tattach(struct o9fsmount *, struct o9fsfid *, char *, char *);
-struct o9fsdir *o9fs_tstat(struct o9fsmount *, char *);
-struct o9fsdir *o9fs_fstat(struct o9fsmount *, struct o9fsfid *);
+struct o9fsstat *o9fs_tstat(struct o9fsmount *, char *);
+struct o9fsstat *o9fs_fstat(struct o9fsmount *, struct o9fsfid *);
 struct o9fsfid *o9fs_twalk(struct o9fsmount *, struct o9fsfid *, char *);
 
 /* o9fs_conv*.c */
@@ -26,7 +24,7 @@ u_int	o9fs_convM2S(u_char*, u_int, struct o9fsfcall*);
 u_int	o9fs_convS2M(struct o9fsfcall*, u_char*, u_int);
 u_int	o9fs_sizeS2M(struct o9fsfcall*);
 int		o9fs_statcheck(u_char *, u_int);
-u_int	o9fs_convM2D(u_char *, u_int, struct o9fsdir *, char *);
+u_int	o9fs_convM2D(u_char *, u_int, struct o9fsstat *, char *);
 
 extern int (**o9fs_vnodeop_p)(void *);
 
