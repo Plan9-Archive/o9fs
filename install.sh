9a72c518708e6e9a6e2b01c1e588ad4ca931bd73
iru <iru@localhost>
2007-08-20 04:03:17 -0300

    .

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 # JUL/2007
 
 # add o9fs' files to kernel compilation
+cp /sys/conf/files /sys/conf/files.old
 sed '/file xfs\/xfs_syscalls-dummy/{p;s/.*/file miscfs\/o9fs\/o9fs_9p\.c              o9fs\
 file miscfs\/o9fs\/o9fs_convM2D\.c         o9fs\
 file miscfs\/o9fs\/o9fs_convM2S\.c         o9fs\
@@ -12,4 +13,6 @@ file miscfs\/o9fs\/o9fs_convS2M\.c         o9fs\
 file miscfs\/o9fs\/o9fs_io\.c              o9fs\
 file miscfs\/o9fs\/o9fs_subr\.c            o9fs\
 file miscfs\/o9fs\/o9fs_vfsops\.c          o9fs\
-file miscfs\/o9fs\/o9fs_vnops\.c           o9fs/;}' /sys/conf/files
+file miscfs\/o9fs\/o9fs_vnops\.c           o9fs/;}' /sys/conf/files #> /sys/conf/files
+
+
