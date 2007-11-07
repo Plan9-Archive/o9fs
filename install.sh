5eb9f34dce8ee52328d92f6c62601ae57004ba20
iru <iru@localhost>
2007-11-07 19:03:53 -0200

    let it be human-readable

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-# installs o9fs stuff in kernel source under /sys
+# installs o9fs stuff in kernel source under /usr/src/sys
 # written by Iruata Souza
 # See LICENSE
 # JUL/2007
 
-# add o9fs' files to kernel compilation
-cp /sys/conf/files /sys/conf/files.old
-sed '/file xfs\/xfs_syscalls-dummy/{p;s/.*/file miscfs\/o9fs\/o9fs_9p\.c              o9fs\
-file miscfs\/o9fs\/o9fs_convM2D\.c         o9fs\
-file miscfs\/o9fs\/o9fs_convM2S\.c         o9fs\
-file miscfs\/o9fs\/o9fs_convS2M\.c         o9fs\
-file miscfs\/o9fs\/o9fs_io\.c              o9fs\
-file miscfs\/o9fs\/o9fs_subr\.c            o9fs\
-file miscfs\/o9fs\/o9fs_vfsops\.c          o9fs\
-file miscfs\/o9fs\/o9fs_vnops\.c           o9fs/;}' /sys/conf/files #> /sys/conf/files
+o9fsroot=miscfs/o9fs
 
+addfiles() {
+	cp /usr/src/sys/conf/files /usr/src/sys/conf/files.old
 
+	[[ -f files.o9fs ]] && rm -f files.o9fs
+
+	for f in *.c; do
+		[[ "$f" == "o9fs_convD2M.c" ]] && continue
+		echo "file $o9fsroot/$f		o9fs" >> files.o9fs
+	done
+
+	exist=$(grep o9fs /usr/src/sys/conf/files)
+	[[ ! -z $exist ]] && exit 0
+
+	echo >> /usr/src/sys/conf/files
+	cat files.o9fs >> /usr/src/sys/conf/files
+}
+
+addfiles
