a932a908492872f74e6c878b5bc621a541f541a1
iru <iru@localhost>
2008-02-12 14:36:11 -0200

    add lkm support

---
+++ o9fs_lkm.c
@@ -0,0 +1,24 @@
+#include <sys/param.h>
+#include <sys/conf.h>
+#include <sys/systm.h>
+#include <sys/ioctl.h>
+#include <sys/mount.h>
+#include <sys/exec.h>
+#include <sys/lkm.h>
+
+#include <miscfs/o9fs/o9fs.h>
+#include <miscfs/o9fs/o9fs_extern.h>
+
+extern struct vnodeopv_desc o9fs_vnodeop_opv_desc;
+extern struct vfsops o9fs_vfsops;
+
+struct vfsconf o9fs_vfsconf = { &o9fs_vfsops, MOUNT_O9FS, 0, 0, 0, NULL, NULL };
+MOD_VFS("o9fs", -1, &o9fs_vfsconf);
+
+int
+o9fs_lkmentry(struct lkm_table *table, int cmd, int ver)
+{
+	vfs_opv_init_explicit(&o9fs_vnodeop_opv_desc);
+	vfs_opv_init_default(&o9fs_vnodeop_opv_desc);
+	DISPATCH(table, cmd, ver, lkm_nofunc, lkm_nofunc, lkm_nofunc);
+}
